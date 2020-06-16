cff3c2761ce8c4c99652e72fccc96b4d140450d6
AnikaZN <anikanacey@gmail.com>
2020-06-16 16:09:49 -0600

    Simplification of code for v.1

--- why_this_song/app.py
+++ why_this_song/app.py
@@ -7,53 +7,23 @@ import os
 
 load_dotenv()
 
-# app = Flask(__name__)
-#
-# credentials = SpotifyClientCredentials(client_id='CLIENT_ID',
-#                                        client_secret='CLIENT_SECRET')
-# sp = spotipy.Spotify(client_credentials_manager=credentials)
-#
-# @app.route('/', methods=['GET'])
-# def index():
-#     return 'Success!'
-#
-# if __name__ == '__main__':
-#     app.run()
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(64)
-app.config['SESSION_TYPE'] = 'filesystem'
 
-Session(app)
-
-auth_manager = spotipy.oauth2.SpotifyOAuth('CLIENT_ID', 'CLIENT_SECRET')
-spotify = spotipy.Spotify(auth_manager=auth_manager)
+client_credentials_manager = SpotifyClientCredentials(client_id='CLIENT_ID',
+                                       client_secret='CLIENT_SECRET')
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    if request.args.get("code"):
-        session['token_info'] = auth_manager.get_access_token(request.args["code"])
-        return redirect('/')
+    return 'Success!'
 
-    if not session.get('token_info'):
-        auth_url = auth_manager.get_authorize_url()
-        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+@app.route('/track', methods=['GET', 'POST'])
+def song():
+    # spotipy.oauth2.SpotifyOauthError: Bad Request at this line
+    results = sp.search(q='weezer', limit=20)
+    for idx, track in enumerate(results['tracks']['items']):
+        print(idx, track['name'])
 
-    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
-           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
-           f'<a href="/playlists">My Playlists</a>'
-
-
-@app.route('/sign_out')
-def sign_out():
-    session.clear()
-    return redirect('/')
-
-
-@app.route('/playlists')
-def playlists():
-    if not session.get('token_info'):
-        return redirect('/')
-    else:
-        return spotify.current_user_playlists()
+if __name__ == '__main__':
+    app.run()
